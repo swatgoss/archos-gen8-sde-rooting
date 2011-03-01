15e790d7267a92aa14f3cbce99e52b5fd8272cd8
Christian Ulrich <chrulri@gmail.com>
2011-03-01 00:46:48 +0000

    initial commit

---
+++ sources/AppDataResizer/src/chrulri/gen8/AppDataResizer/MainActivity.java
@@ -0,0 +1,13 @@
+package chrulri.gen8.AppDataResizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
