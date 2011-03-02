1bf29c49cdd37040cfee1675aaafc2ad27200658
Christian Ulrich <chrulri@gmail.com>
2011-03-02 01:27:45 +0000

    extended utilities with IMountService (non-public API)

--- sources/AppDataResizer/src/chrulri/gen8/AppDataResizer/MainActivity.java
+++ sources/AppDataResizer/src/chrulri/gen8/AppDataResizer/MainActivity.java
@@ -4,11 +4,15 @@ import java.io.File;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
+import chrulri.gen8.AppDataResizer.Utils.FileUtils;
+import chrulri.gen8.AppDataResizer.Utils.MountService;
+import chrulri.gen8.AppDataResizer.Utils.ProcessUtils;
 
 public class MainActivity extends Activity {
 
@@ -20,11 +24,41 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		// register button click handler
 		Button btnTest = (Button) findViewById(R.id.btnTest);
 		btnTest.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				testParted();
+			}
+		});
+		Button btnUnmount = (Button) findViewById(R.id.btnUnmount);
+		btnUnmount.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Object msvc = MountService.getService();
+				String sdc = Environment.getExternalStorageDirectory().toString();
+				MountService.unmountVolume(msvc, sdc, false);
+			}
+		});
+		Button btnMount = (Button) findViewById(R.id.btnMount);
+		btnMount.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Object msvc = MountService.getService();
+				String sdc = Environment.getExternalStorageDirectory().toString();
+				MountService.mountVolume(msvc, sdc);
+			}
+		});
+		Button btnCheck = (Button) findViewById(R.id.btnCheck);
+		btnCheck.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Object msvc = MountService.getService();
+				String sdc = Environment.getExternalStorageDirectory().toString();
+				toast(sdc + Utils.NEWLINE + MountService.getVolumeState(msvc, sdc)
+						+ Utils.NEWLINE
+						+ Boolean.toString(MountService.isUsbMassStorageEnabled(msvc)));
 			}
 		});
 	}
@@ -40,22 +74,32 @@ public class MainActivity extends Activity {
 	private void testParted() {
 		File partedBinary = getFileStreamPath(PARTED_BIN);
 		partedBinary.delete();
-		if (!Utils.copyToFile(getResources().openRawResource(R.raw.parted),
+		if (!FileUtils.copyToFile(getResources().openRawResource(R.raw.parted),
 				partedBinary)) {
 			toast(R.string.err_extract);
 			return;
 		}
 		try {
-			int ret = Utils.runAsRoot("chmod", "755", partedBinary.getAbsolutePath())
-					.waitFor();
+			int ret = ProcessUtils.runAsRoot("chmod", "755",
+					partedBinary.getAbsolutePath()).waitFor();
 			if (ret != 0) {
 				Log.e(TAG, "chmod (" + ret + ")");
 				toast(R.string.err_chmod);
 			}
-			Process p1 = Utils.runAsRoot(partedBinary.getAbsolutePath(),
-					INTERNAL_DEVICE, "print", "list");
-			String partedOutput = Utils.readProcess(p1);
+			Process p1 = ProcessUtils.runAsRoot(partedBinary.getAbsolutePath(),
+					"-ms", INTERNAL_DEVICE, "print");
+			String partedOutput = ProcessUtils.readStdOut(p1);
 			toast(partedOutput);
+			// TODO check partitions:
+			// parted.bin -ms /dev/block/... check 1
+			// parted.bin -ms /dev/block/... check 2
+			// TODO resize:
+			// parted.bin -ms /dev/block/... resize 1 START_OF_PART_ONE END_OF_DEVICE-1000
+			// TODO mkpart:
+			// parted.bin -ms /dev/block/... mkpart primary END_OF_PART_ONE END_OF_DEVICE
+			// TODO mkfs:
+			// mke2fs -Text3 /dev/block/...p2
+			// tune2fs -c-1 -i0 /dev/block/...p2
 		} catch (Exception e) {
 			Log.e(TAG, "parted", e);
 			toast(R.string.err_parted);
--- sources/AppDataResizer/src/chrulri/gen8/AppDataResizer/Utils.java
+++ sources/AppDataResizer/src/chrulri/gen8/AppDataResizer/Utils.java
@@ -5,102 +5,198 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import android.os.IBinder;
+import android.util.AndroidRuntimeException;
 import android.util.Log;
 
 final class Utils {
 	static final String TAG = Utils.class.getName();
+
 	static final int BUFFER_SIZE = 10240;
+	static final String SERVICE_MOUNT = "mount";
 
 	public static final String NEWLINE = System.getProperty("line.separator");
-	private static Method METHOD_COPYFILE;
-	private static Method METHOD_COPYTOFILE;
-	private static Method METHOD_SETPERMISSIONS;
+
+	private static Method METHOD_FileUtils_copyFile;
+	private static Method METHOD_FileUtils_copyToFile;
+	private static Method METHOD_FileUtils_setPermissions;
+	private static Method METHOD_ServiceManager_getService;
+	private static Method METHOD_IMountService_asInterface;
+	private static Method METHOD_IMountService_getVolumeState;
+	private static Method METHOD_IMountService_mountVolume;
+	private static Method METHOD_IMountService_unmountVolume;
+	private static Method METHOD_IMountService_getStorageUsers;
+	private static Method METHOD_IMountService_isUsbMassStorageEnabled;
 
 	static {
 		try {
-			Class<?> clazz = Class.forName("android.os.FileUtils");
-			METHOD_COPYFILE = clazz.getMethod("copyFile", File.class, File.class);
-			METHOD_COPYTOFILE = clazz.getMethod("copyToFile", InputStream.class,
+			Class<?> clazz;
+			clazz = Class.forName("android.os.FileUtils");
+			METHOD_FileUtils_copyFile = clazz.getMethod("copyFile", File.class,
 					File.class);
-			METHOD_SETPERMISSIONS = clazz.getMethod("setPermissions", String.class,
-					int.class, int.class, int.class);
-		} catch (Throwable t) {
-			Log.wtf(TAG, t);
+			METHOD_FileUtils_copyToFile = clazz.getMethod("copyToFile",
+					InputStream.class, File.class);
+			METHOD_FileUtils_setPermissions = clazz.getMethod("setPermissions",
+					String.class, int.class, int.class, int.class);
+
+			clazz = Class.forName("android.os.ServiceManager");
+			METHOD_ServiceManager_getService = clazz.getMethod("getService",
+					String.class);
+
+			clazz = Class.forName("android.os.storage.IMountService$Stub");
+			METHOD_IMountService_asInterface = clazz.getMethod("asInterface",
+					IBinder.class);
+
+			clazz = Class.forName("android.os.storage.IMountService");
+			METHOD_IMountService_getVolumeState = clazz.getMethod("getVolumeState",
+					String.class);
+			METHOD_IMountService_mountVolume = clazz.getMethod("mountVolume",
+					String.class);
+			METHOD_IMountService_unmountVolume = clazz.getMethod("unmountVolume",
+					String.class, boolean.class);
+			METHOD_IMountService_getStorageUsers = clazz.getMethod("getStorageUsers",
+					String.class);
+			METHOD_IMountService_isUsbMassStorageEnabled = clazz
+					.getMethod("isUsbMassStorageEnabled");
+		} catch (Exception e) {
+			Log.wtf(TAG, e);
+		}
+	}
+
+	private static Object invoke(Method method, Object receiver, Object... args) {
+		try {
+			return method.invoke(receiver, args);
+		} catch (IllegalArgumentException e) {
+			Log.e(TAG, null, e);
+			throw new AndroidRuntimeException(e);
+		} catch (IllegalAccessException e) {
+			Log.e(TAG, null, e);
+			throw new AndroidRuntimeException(e);
+		} catch (InvocationTargetException e) {
+			Log.e(TAG, null, e);
+			throw new AndroidRuntimeException(e);
 		}
 	}
 
 	public static String getStackTrace(final Throwable t) {
 		if (t == null)
 			return "null";
-		final StringWriter buf = new StringWriter();
-		final PrintWriter writer = new PrintWriter(buf);
+		StringWriter buf = new StringWriter();
+		PrintWriter writer = new PrintWriter(buf);
 		t.printStackTrace(writer);
 		return buf.toString();
 	}
 
-	public static boolean copyFile(File srcFile, File destFile) {
-		try {
-			return (Boolean) METHOD_COPYFILE.invoke(null, srcFile, destFile);
-		} catch (Exception e) {
-			Log.e(TAG, null, e);
-			return false;
+	public static class FileUtils {
+
+		public static boolean copyFile(File srcFile, File destFile) {
+			return (Boolean) invoke(METHOD_FileUtils_copyFile, null, srcFile,
+					destFile);
+		}
+
+		public static boolean copyToFile(InputStream inputStream, File destFile) {
+			return (Boolean) invoke(METHOD_FileUtils_copyToFile, null, inputStream,
+					destFile);
+		}
+
+		public static int setPermissions(String file, int mode, int uid, int gid) {
+			return (Integer) invoke(METHOD_FileUtils_setPermissions, null, file,
+					mode, uid, gid);
 		}
 	}
 
-	public static boolean copyToFile(InputStream inputStream, File destFile) {
-		try {
-			return (Boolean) METHOD_COPYTOFILE.invoke(null, inputStream, destFile);
-		} catch (Exception e) {
-			Log.e(TAG, null, e);
-			return false;
+	public static class ProcessUtils {
+
+		public static Process run(String... args) throws IOException {
+			return Runtime.getRuntime().exec(args);
+		}
+
+		public static Process runAsRoot(String... args) throws IOException {
+			if (args == null || args.length == 0)
+				return null;
+			Process su = run("su");
+			DataOutputStream out = new DataOutputStream(su.getOutputStream());
+			for (String arg : args) {
+				out.writeBytes(" ");
+				out.writeBytes(arg);
+			}
+			out.writeBytes("\n");
+			out.writeBytes("exit\n");
+			out.flush();
+			return su;
+		}
+
+		public static String readStdOut(Process proc) throws IOException {
+			InputStream pin = proc.getInputStream();
+			try {
+				proc.waitFor();
+			} catch (InterruptedException e) {
+			}
+			Writer out = new StringWriter();
+			char[] buf = new char[BUFFER_SIZE];
+			Reader in = new InputStreamReader(pin);
+			int len;
+			while ((len = in.read(buf)) != -1) {
+				out.write(buf, 0, len);
+			}
+			return out.toString();
+		}
+
+		public static void printLine(Process proc, String line) throws IOException {
+			OutputStream pout = proc.getOutputStream();
+			Writer out = new OutputStreamWriter(pout);
+			out.write(line);
+			out.write(NEWLINE);
+			out.flush();
 		}
 	}
 
-	public static int setPermissions(String file, int mode, int uid, int gid) {
-		try {
-			return (Integer) METHOD_SETPERMISSIONS.invoke(null, file, mode, uid, gid);
-		} catch (Exception e) {
-			Log.e(TAG, null, e);
-			return -1;
-		}
-	}
+	public static class MountService {
 
-	public static Process run(String... args) throws IOException {
-		return Runtime.getRuntime().exec(args);
-	}
-
-	public static Process runAsRoot(String... args) throws IOException {
-		if (args == null || args.length == 0)
+		public static Object getService() {
+			Object service = invoke(METHOD_ServiceManager_getService, null,
+					SERVICE_MOUNT);
+			if (service != null) {
+				return invoke(METHOD_IMountService_asInterface, null, service);
+			} else {
+				Log.e(TAG, "Can't get mount service");
+			}
 			return null;
-		Process su = run("su");
-		DataOutputStream out = new DataOutputStream(su.getOutputStream());
-		for (String arg : args) {
-			out.writeBytes(" ");
-			out.writeBytes(arg);
 		}
-		out.writeBytes("\n");
-		out.writeBytes("exit\n");
-		out.flush();
-		return su;
-	}
 
-	public static String readProcess(Process p1) throws InterruptedException,
-			IOException {
-		InputStream pin = p1.getInputStream();
-		p1.waitFor();
-		Writer out = new StringWriter();
-		char[] buf = new char[BUFFER_SIZE];
-		Reader in = new InputStreamReader(pin);
-		int len;
-		while ((len = in.read(buf)) != -1) {
-			out.write(buf, 0, len);
+		public static String getVolumeState(Object mountService, String mountPoint) {
+			return (String) invoke(METHOD_IMountService_getVolumeState, mountService,
+					mountPoint);
 		}
-		return out.toString();
+
+		public static int mountVolume(Object mountService, String mountPoint) {
+			return (Integer) invoke(METHOD_IMountService_mountVolume, mountService,
+					mountPoint);
+		}
+
+		public static void unmountVolume(Object mountService, String mountPoint,
+				boolean force) {
+			invoke(METHOD_IMountService_unmountVolume, mountService, mountPoint,
+					force);
+		}
+
+		public static int[] getStorageUsers(Object mountService, String path) {
+			return (int[]) invoke(METHOD_IMountService_getStorageUsers, mountService,
+					path);
+		}
+
+		public static boolean isUsbMassStorageEnabled(Object mountService) {
+			return (Boolean) invoke(METHOD_IMountService_isUsbMassStorageEnabled,
+					mountService);
+		}
 	}
 }
