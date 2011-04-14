6be695c7f9520a904dead2f0fedf79350897aacb
Christian Ulrich <chrulri@gmail.com>
2011-04-14 20:34:49 +0000

    fixed compatibility for python 3.1

--- scripts/avos_dump.py
+++ scripts/avos_dump.py
@@ -10,34 +10,34 @@ def readuint(inb,offset):
 
 def main():
 	if (len(sys.argv) != 2):
-		print "usage: " + sys.argv[0] + " FILE"
+		print('usage: ' + sys.argv[0] + ' FILE')
 		sys.exit(2)
 	inf=sys.argv[1]
 	kf='zImage'
 	rf='initramfs.cpio.gz'
-	print "loading '"+inf+"' ..."
+	print('loading \''+inf+'\' ...')
 	ib = open(inf,'r+b')
 	# read kernel size
 	ksize = readuint(ib,0x94) - 256
-	print "kernel size: %i" % ksize
+	print('kernel size: %i' % ksize)
 	# read initramfs size
 	rsize = readuint(ib,0x98)
-	print "initramfs size: %i" % rsize
+	print('initramfs size: %i' % rsize)
 	# write kernel
-	print "extracting "+kf+" ..."
+	print('extracting \''+kf+'\' ...')
 	ob = open(kf,'w+b')
 	ib.seek(256)
 	for i in range(0,ksize):
 		ob.write(ib.read(1))
 	ob.close()
 	# write initramfs
-	print "extracting "+rf+" ..."
+	print('extracting \''+rf+'\' ...')
 	ob = open(rf,'w+b')
 	for i in range(0,rsize):
 		ob.write(ib.read(1))
 	ob.close()
 	ib.close()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
 
